490abb25688fcf48a58b0bc823564f0d48b4ff8d
Braeden Grimes <bdgrimes0@gmail.com>
2022-10-31 21:27:34 -0500

    Added basic error boundry fallback.

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,31 @@ import { MovieRoutes } from './features/movies';
 import { MainLayout } from './components/Layout';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './libs/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Button } from '@mantine/core';
+
+const ErrorFallback = () => {
+  return (
+    <div>
+      <h2>Oops! Something went wrong</h2>;
+      <Button onClick={() => window.location.assign(window.location.origin)}>Refresh</Button>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <MantineProvider>
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<MovieRoutes />} />
-            </Route>
-          </Routes>
-        </QueryClientProvider>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/" element={<MainLayout />}>
+                <Route index element={<MovieRoutes />} />
+              </Route>
+            </Routes>
+          </QueryClientProvider>
+        </ErrorBoundary>
       </BrowserRouter>
     </MantineProvider>
   );
