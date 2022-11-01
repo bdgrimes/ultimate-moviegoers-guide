55b358ce01aa2eb58a68073868eed113e83b9feb
Braeden Grimes <bdgrimes0@gmail.com>
2022-10-31 21:06:56 -0500

    Added react-query. Wrapped app in QueryClientProvider to access react-query.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { MovieRoutes } from './features/movies';
 import { MainLayout } from './components/Layout';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './libs/react-query';
 
 const App = () => {
   return (
     <MantineProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<MovieRoutes />} />
-          </Route>
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<MovieRoutes />} />
+            </Route>
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </MantineProvider>
   );
