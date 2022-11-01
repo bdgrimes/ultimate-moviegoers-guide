6f74ca899ddd094b286373531c892d204ff08f03
Braeden Grimes <bdgrimes0@gmail.com>
2022-10-31 21:56:55 -0500

    Added return type for get now playing api query.

--- src/features/movies/api/getNowPlaying.ts
+++ src/features/movies/api/getNowPlaying.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
+import { NowPlaying } from '../types/NowPlaying';
 
-export const getNowPlaying = async () => {
+export const getNowPlaying = async (): Promise<NowPlaying> => {
   const res = await fetch(
     'https://api.themoviedb.org/3/movie/now_playing?api_key=e2f91aa33bd87c356d18db8b8978fe44&language=en-US&page=1'
   );
