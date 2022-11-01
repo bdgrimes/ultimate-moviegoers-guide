e0802e213e1c72d18ade940ff7d4af0fd1998099
Braeden Grimes <bdgrimes0@gmail.com>
2022-10-31 20:33:30 -0500

    Added main layout as parent element with an outlet for children routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { MovieRoutes } from './features/movies';
+import { MainLayout } from './components/Layout';
 
 const App = () => {
   return (
     <MantineProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MovieRoutes />} />
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<MovieRoutes />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </MantineProvider>
