3804df7c68c99c3c1d7f16ab1c08a88c18d90ea7
Braeden Grimes <bdgrimes0@gmail.com>
2022-11-01 22:02:53 -0500

    Fixed rating display

--- src/features/movies/components/MovieRating.tsx
+++ src/features/movies/components/MovieRating.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 export const MovieRating = ({ vote_average }: Props) => {
-  const voteAverageAsFiveStarCount = (vote_average || 0 * 10) / 20;
+  const voteAverageAsFiveStarCount = ((vote_average || 0) * 10) / 20;
 
   return <Rating fractions={2} value={voteAverageAsFiveStarCount} readOnly />;
 };
