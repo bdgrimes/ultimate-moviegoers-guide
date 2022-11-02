5cb04ea1f550e3f99514731956d0958042abe9db
Braeden Grimes <braeden.grimes@lptech.com>
2022-11-02 09:24:38 -0500

    Added gradient to home page text.

--- src/features/misc/routes/Home.tsx
+++ src/features/misc/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { Grid, Title, Space, Text } from '@mantine/core';
+import { Grid, Title, Space } from '@mantine/core';
 import { ReactComponent as MovieSvg } from '../../../assets/undraw_home_cinema_l7yl.svg';
 
 export const Home = () => {
@@ -8,7 +8,12 @@ export const Home = () => {
         <MovieSvg style={{ maxHeight: '500px' }} />
       </Grid.Col>
       <Grid.Col xl={6} lg={12}>
-        <Title order={1} weight={300}>
+        <Title
+          order={1}
+          weight={300}
+          variant="gradient"
+          gradient={{ from: 'indigo', to: 'blue', deg: 45 }}
+        >
           Welcome to the <b>ultimate</b> movie goers guide.
           <Space h="md" />
           <Title order={2} weight={200}>
